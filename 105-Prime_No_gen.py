50173677b2a78bbd5fc83a14f9eb514bd0ba47dd
V.K. Saketh <43966992+shades-of-blue@users.noreply.github.com>
2020-10-01 20:53:59 +0530

    Update 105-Prime_No_gen.py

--- 105-Prime_No_gen.py
+++ 105-Prime_No_gen.py
@@ -1,7 +1,7 @@
 # Python program to display all the prime numbers within an interval
 
-lower = 900
-upper = 1000
+lower =  int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
 
 print("Prime numbers between", lower, "and", upper, "are:")
 
